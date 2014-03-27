719104e587224282a261ece3f764f3780893f0f9
sahb <WIN-BFM6JJ47JRD\sahb>
2014-03-27 02:02:53 -0700

    Administrationssystem

---
+++ ARK/Filter.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Administrationssystem
+{
+    /// <summary>
+    /// Interaction logic for Filter.xaml
+    /// </summary>
+    public partial class Filter : UserControl
+    {
+        List<FilterControl> filterControls = new List<FilterControl>();
+        public Filter()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 10; i++)
+                ListBox.Items.Add(new FilterControl("FilterNavn"));
+        }
+    }
+}
