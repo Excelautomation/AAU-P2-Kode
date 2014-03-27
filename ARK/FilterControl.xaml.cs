da414fd37341c2f8196c54e1d0a4bf3b5aba0018
Søren Bjergmark <sahb1239@gmail.com>
2014-03-27 02:02:53 -0700

    Administrationssystem

---
+++ ARK/FilterControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Administrationssystem
+{
+    /// <summary>
+    /// Interaction logic for FilterControl.xaml
+    /// </summary>
+    public partial class FilterControl : UserControl
+    {
+        public string FilterName { get { return this.UISelecter.Text; } set { this.UISelecter.Text = value; } }
+
+        public FilterControl(string filterName)
+        {
+            InitializeComponent();
+
+            this.FilterName = filterName;
+        }
+    }
+}
