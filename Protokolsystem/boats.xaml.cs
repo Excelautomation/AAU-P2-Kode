af68bf147f6f54cc7c4779775e7f41781f579051
mclc <WIN-BFM6JJ47JRD\mclc>
2014-03-27 08:51:59 -0700

    Tilføjet protokolsystem

---
+++ Protokolsystem/boats.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Protokolsystem
+{
+	/// <summary>
+	/// Interaction logic for Listview_baade.xaml
+	/// </summary>
+	public partial class boats : UserControl
+	{
+		public boats()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
