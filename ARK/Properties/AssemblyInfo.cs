1ca0097a494609e214dbb2ca81318318a147e3f1
sahb <WIN-BFM6JJ47JRD\sahb>
2014-03-28 01:26:08 -0700

    Ændret information

--- ARK/Properties/AssemblyInfo.cs
+++ ARK/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ARK")]
+[assembly: AssemblyTitle("Aalborg Roklub")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ARK")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("Aalborg Roklub")]
+[assembly: AssemblyCopyright("Copyright © A304 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
