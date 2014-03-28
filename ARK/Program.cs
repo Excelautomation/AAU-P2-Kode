d6fb47ccf793b46e474ef8171980148f783f2554
Søren Bjergmark <sahb1239@gmail.com>
2014-03-28 01:15:26 -0700

    Tilføjet program fil

---
+++ ARK/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ARK
+{
+    class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Window window;
+
+            if (args.Select(e => e.ToLower()).Contains("administrationssystem"))
+            {
+                window = new Administrationssystem();
+            } else
+            {
+                window = new Administrationssystem();
+            }
+
+            var app = new Application();
+            app.Run(window);
+        }
+    }
+}
