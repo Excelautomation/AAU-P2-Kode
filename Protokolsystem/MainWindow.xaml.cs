0038b639ee75fc7486d19bcde56b4a0ce24716e3
Jimmi <WIN-BFM6JJ47JRD\Jimmi>
2014-03-30 10:02:41 -0700

    Designing and shit

--- Protokolsystem/MainWindow.xaml.cs
+++ Protokolsystem/MainWindow.xaml.cs
@@ -24,5 +24,68 @@ namespace Protokolsystem
         {
             InitializeComponent();
         }
+
+        private void BeginTrip_Click(object sender, RoutedEventArgs e)
+        {
+            BeginTripBoats boatCollection = new BeginTripBoats();
+
+            Content.Children.Clear();
+            Content.Children.Add(boatCollection);
+        }
+
+        private void EndTrip_Click(object sender, RoutedEventArgs e)
+        {
+            EndTrip boatEndtrip = new EndTrip();
+
+            Content.Children.Clear();
+            Content.Children.Add(boatEndtrip);
+        }
+
+        private void boatsOverview_Click(object sender, RoutedEventArgs e)
+        {
+            BoatsOut boatOut = new BoatsOut();
+
+            Content.Children.Clear();
+            Content.Children.Add(boatOut);
+        }
+
+        private void DistanceStatistics_Click(object sender, RoutedEventArgs e)
+        {
+            DistanceStatistics distanceMembers = new DistanceStatistics();
+
+            Content.Children.Clear();
+            Content.Children.Add(distanceMembers);
+        }
+
+        private void MembersInformation_Click(object sender, RoutedEventArgs e)
+        {
+            MembersInformation membersInfo = new MembersInformation();
+
+            Content.Children.Clear();
+            Content.Children.Add(membersInfo);
+        }
+
+        private void CreateInjury_Click(object sender, RoutedEventArgs e)
+        {
+            CreateInjury injury = new CreateInjury();
+
+            Content.Children.Clear();
+            Content.Children.Add(injury);
+        }
+
+        private void CreateLongDistance_Click(object sender, RoutedEventArgs e)
+        {
+            CreateLongDistance longDistance = new CreateLongDistance();
+
+            Content.Children.Clear();
+            Content.Children.Add(longDistance);
+        }
+
+        private void AdminPanel_Click(object sender, RoutedEventArgs e)
+        {
+            AdminLogin admin = new AdminLogin();
+
+            admin.Show();
+        }
     }
 }
