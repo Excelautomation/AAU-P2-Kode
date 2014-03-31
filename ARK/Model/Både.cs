46ae2918e1e0df12521a8cd53864ef0e0d2132db
Søren Bjergmark <sahb1239@gmail.com>
2014-03-31 13:39:24 -0700

    Oprettet testprojekt Opdateret model med både

--- ARK/Model/Både.cs
+++ ARK/Model/Både.cs
@@ -6,14 +6,15 @@ using System.Threading.Tasks;
 
 namespace ARK.Model
 {
-    class Både
+    public class Både
     {
-        public int Id { get; set; }
+        public int ID { get; set; }
         public string Navn { get; set; }
+        public int AntalPladser { get; set; }
         public bool Aktiv { get; set; }
-        public int Bådtype { get; set; }
+        public int BådType { get; set; }
         public bool Roforbud { get; set; }
-        public int Specifikbådtype { get; set; }
-        public bool Langtursbåd { get; set; }
+        public int SpecifikBådType { get; set; }
+        public bool LangtursBåd { get; set; }
     }
 }
