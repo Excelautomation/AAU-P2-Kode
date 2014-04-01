b8cea6c6eecf348075206ccb8122f1aed538ab1d
Jonas <WIN-BFM6JJ47JRD\Jonas>
2014-03-31 19:06:10 -0700

    Medlem.cs tilføjet

---
+++ ARK/Model/Medlem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARK.Model
+{
+    class Medlem
+    {
+        public int MedlemsNr { get; set; }
+        public int ID { get; set; }
+        public string Fornavn { get; set; }
+        public string Efternavn { get; set; }
+        public string Adresse1 { get; set; }
+        public string Adresse2 { get; set; }
+        public int PostNr { get; set; }
+        public string By { get; set; }
+        public string Telefon { get; set; }
+        public string TelefonArbejde { get; set; }
+        public string TelefonMobil { get; set; }
+        public string Email1 { get; set; }
+        public string Email2 { get; set; }
+        public DateTime Fødselsdato { get; set; }
+        public bool Frigivet { get; set; }
+        public bool Svømmeprøve { get; set; }
+        public bool KortTStyrmand { get; set; }
+        public bool LangTStyrmand { get; set; }
+        public bool ScullerRet { get; set; }
+        public bool OutriggerRet { get; set; }
+        public bool Kajakret { get; set; }
+    }
+}
