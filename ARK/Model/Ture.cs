6c7bd3a325d7d8f48b51277c5d038a46ed318596
Jimmi <WIN-BFM6JJ47JRD\Jimmi>
2014-04-02 05:54:31 -0700

    Ture.cs (Jimmi Nielsen)

---
+++ ARK/Model/Ture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARK.Model
+{
+    class Ture
+    {
+        public int ID { get; set; }
+        public int Kilometer { get; set; }
+        public DateTime Dato { get; set; }
+        public Boolean Langtur { get; set; }
+        public int BådID { get; set; }
+        public int MedlemsID_1 { get; set; }
+        public int MedlemsID_2 { get; set; }
+        public int MedlemsID_3 { get; set; }
+        public int MedlemsID_4 { get; set; }
+        public int MedlemsID_5 { get; set; }
+        public int MedlemsID_6 { get; set; }
+        public int MedlemsID_7 { get; set; }
+        public int MedlemsID_8 { get; set; }
+        public int MedlemsID_9 { get; set; }
+    }
+}
