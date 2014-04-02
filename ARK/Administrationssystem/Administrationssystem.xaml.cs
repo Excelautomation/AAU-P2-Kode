a9e646da626c35465db3c0e12fa11b8bab47e8e7
Myrionsc2 <marpht@hotmail.com>
2014-04-02 05:47:51 -0700

    Søgning og Filterboks. Søren og Mads' arbejde udkommenteret. Grå baggrund.

--- ARK/Administrationssystem/Administrationssystem.xaml.cs
+++ ARK/Administrationssystem/Administrationssystem.xaml.cs
@@ -27,8 +27,38 @@ namespace ARK.Administrationssystem
         {
             InitializeComponent();
 
-            for (int i = 0; i < 4; i++)
-                Filters.Children.Add(new FilterControl("FilterNavn" + i));
+            //for (int i = 0; i < 4; i++)
+            //    Filters.Children.Add(new FilterControl("FilterNavn" + i));
+
+            CheckBox[] arrch = new CheckBox[] {
+                new CheckBox() { Name="ch1", Content="checkbox1", FontSize=16 },
+                new CheckBox() { Name="ch2", Content="checkbox2", FontSize=16 },
+                new CheckBox() { Name="ch3", Content="checkbox3", FontSize=16 },
+                new CheckBox() { Name="ch4", Content="checkbox4", FontSize=16 }
+            };
+
+            foreach (CheckBox ch in arrch)      
+            {
+                Stackpanelfilter.Children.Add(ch);
+            }
+            
+        }
+
+        // Skift vinduer i maincontent
+        private void btnMenuHome_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Children.Clear();
+            MainContent.Children.Add(new Oversigt());
+        }
+        private void btnMenuBlanketter_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Children.Clear();
+            MainContent.Children.Add(new Pages.Blanketter());
+        }
+        private void btnMenuIndstillinger_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Children.Clear();
+            MainContent.Children.Add(new Pages.Indstillinger());
         }
     }
 }
