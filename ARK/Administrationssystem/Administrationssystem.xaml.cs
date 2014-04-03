9eb012434703fe367ea9669292a00e115ec4ee94
sahb <WIN-BFM6JJ47JRD\sahb>
2014-04-03 03:48:01 -0700

    Ændret administrationssystem til at benytte viewmodel

--- ARK/Administrationssystem/Administrationssystem.xaml.cs
+++ ARK/Administrationssystem/Administrationssystem.xaml.cs
@@ -20,85 +20,13 @@ namespace ARK.Administrationssystem
     /// <summary>
     /// Interaction logic for Administrationssystem.xaml
     /// </summary>
-    public partial class Administrationssystem : Window, INotifyPropertyChanged
+    public partial class Administrationssystem : Window
     {
-        private ObservableCollection<CheckBox> _filterControls;
-        public ObservableCollection<CheckBox> FilterControls { get { return _filterControls; } set { _filterControls = value; Notify("FilterControls"); } }
-
         public Administrationssystem()
         {
             InitializeComponent();
 
-            this.DataContext = this;
-
-            FilterControls = new ObservableCollection<CheckBox>() {
-                new CheckBox() { Name="oversigt_cb1", Content="Både på vandet"},
-                new CheckBox() { Name="oversigt_cb2", Content="Skadesblanketter"},
-                new CheckBox() { Name="oversigt_cb3", Content="Langtursansøgninger"},
-                new CheckBox() { Name="oversigt_cb4", Content="Bådtype1" },
-                new CheckBox() { Name="oversigt_cb5", Content="Bådtype2"},
-                new CheckBox() { Name="oversigt_cb6", Content="Bådtype3"},
-                new CheckBox() { Name="oversigt_cb7", Content="Bådtype4"}
-            };
+            this.DataContext = new ARK.ViewModel.AdministrationssystemViewModel();
         }
-
-        // Skift vinduer i maincontent
-        private void btnMenuHome_Click(object sender, RoutedEventArgs e)
-        {
-            MainContent.Children.Clear();
-            MainContent.Children.Add(new Oversigt());
-
-            // duplikeret fra Administrationssystem(). Grimt grimt.
-            var oversigt_checkbox_arr = new CheckBox[] {
-                new CheckBox() { Name="oversigt_cb1", Content="Både på vandet"},
-                new CheckBox() { Name="oversigt_cb2", Content="Skadesblanketter"},
-                new CheckBox() { Name="oversigt_cb3", Content="Langtursansøgninger"},
-                new CheckBox() { Name="oversigt_cb4", Content="Bådtype1" },
-                new CheckBox() { Name="oversigt_cb5", Content="Bådtype2"},
-                new CheckBox() { Name="oversigt_cb6", Content="Bådtype3"},
-                new CheckBox() { Name="oversigt_cb7", Content="Bådtype4"}
-            };
-
-            Stackpanelfilter.Children.Clear();
-            foreach (CheckBox cb in oversigt_checkbox_arr)
-            {
-                Stackpanelfilter.Children.Add(cb);
-            }
-        }
-        private void btnMenuBlanketter_Click(object sender, RoutedEventArgs e)
-        {
-            MainContent.Children.Clear();
-            MainContent.Children.Add(new Pages.Blanketter());
-
-            var Blanketter_checkbox_arr = new CheckBox[] {
-                new CheckBox() { Name="blank_cb1", Content="Skadesblanketter"},
-                new CheckBox() { Name="blank_cb2", Content="Langtursblanketter"},
-                new CheckBox() { Name="blank_cb3", Content="Ulæste"},
-                new CheckBox() { Name="blank_cb4", Content="Læste"},
-            };
-
-            Stackpanelfilter.Children.Clear();
-            foreach (CheckBox cb in Blanketter_checkbox_arr)
-            {
-                Stackpanelfilter.Children.Add(cb);
-            }
-
-        }
-        private void btnMenuIndstillinger_Click(object sender, RoutedEventArgs e)
-        {
-            MainContent.Children.Clear();
-            MainContent.Children.Add(new Pages.Indstillinger());
-        }
-
-        #region Property
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected void Notify(string propertyName)
-        {
-            if (this.PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-        #endregion
     }
 }
