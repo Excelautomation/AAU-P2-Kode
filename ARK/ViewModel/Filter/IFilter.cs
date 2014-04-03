09130ec265969902741cf1ebf474ea731f8a7c9f
sahb <WIN-BFM6JJ47JRD\sahb>
2014-04-03 05:20:01 -0700

    Rykket delegatecommand ind i seperat klasse TimeCounter Filter

---
+++ ARK/ViewModel/Filter/IFilter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ARK.ViewModel.Filter
+{
+    interface IFilter<T> : ICollection<IFilterItems<T>>
+    {
+        bool FilterItem(T item);
+        IEnumerable<T> FilterItems(T[] items);
+    }
+}
---
+++ ARK/ViewModel/TimeCounter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARK.ViewModel
+{
+    class TimeCounter
+    {
+        private static Stack<DateTime> stack = new Stack<DateTime>();
+        public static void startTimer()
+        {
+            stack.Push(DateTime.Now);
+        }
+
+        public static TimeSpan stopTime()
+        {
+            return DateTime.Now - stack.Pop();
+        }
+
+        public static void stopTime(string name)
+        {
+            TimeSpan span = stopTime();
+
+            Debug.WriteLine(name + " took " + span.TotalMilliseconds + " ms to execute");
+        }
+    }
+}
