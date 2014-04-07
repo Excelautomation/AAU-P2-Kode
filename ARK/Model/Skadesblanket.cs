6ea75f2908f4b184791526636f413f7631720d3e
Jonas <WIN-BFM6JJ47JRD\Jonas>
2014-04-07 08:43:44 -0700

    SkadeBlanket tilføjet

---
+++ ARK/Model/Skadesblanket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARK.Model
+{
+    public class Skadesblanket : Search.Searchable<Skadesblanket>
+    {
+        public DateTime Dato { get; set; }
+        public string AnmeldersNavn { get; set; }
+        public int AnmeldersMedlemsNr { get; set; }
+        public Baad SkadetBåd { get; set; }
+        public SkadeBeskrivelse Skade { get; set; }
+    }
+
+    public class SkadeBeskrivelse
+    {
+        public string SkadeBeskrivelse { get; set; }
+        public string PåkrævedeMaterialer { get; set; }
+    }
+}
