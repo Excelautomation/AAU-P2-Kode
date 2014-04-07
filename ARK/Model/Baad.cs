97197e85c79f0886db144bd2379fb1a00505c0ef
Søren Bjergmark <sahb1239@gmail.com>
2014-04-07 11:35:29 -0700

    Fixet CSProj, camt baad og skadesblanket

---
+++ ARK/Model/Baad.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARK.Model
+{
+    public class Baad : Search.Searchable<Baad>
+    {
+        public int ID { get; set; }
+        public string Navn { get; set; }
+        public int AntalPladser { get; set; }
+        public bool Aktiv { get; set; }
+        public int BådType { get; set; }
+        public bool Roforbud { get; set; }
+        public int SpecifikBådType { get; set; }
+        public bool LangtursBåd { get; set; }
+
+        public override Baad getTarget()
+        {
+            return this;
+        }
+    }
+}
