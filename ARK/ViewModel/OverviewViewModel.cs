fb38333cc736c4a79a82fadd54f6b5ca026a1e51
sahb <WIN-BFM6JJ47JRD\sahb>
2014-04-09 13:22:08 -0700

    Tilføjet overview viewmodel Ændret namespace

---
+++ ARK/ViewModel/OverviewViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ARK.Model;
+
+namespace ARK.ViewModel
+{
+    public class OverviewViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Skadesblanket> _skadesblanketter = new ObservableCollection<Skadesblanket>();
+        public ObservableCollection<Skadesblanket> Skadesblanketter { get { return _skadesblanketter; } set { _skadesblanketter = value; Notify("Skadesblanketter"); } }
+
+        public OverviewViewModel()
+        {
+            Skadesblanketter.Add(new Skadesblanket() { AnmeldersNavn = "Martin er noob" });
+            Skadesblanketter.Add(new Skadesblanket {AnmeldersNavn = "Martin er mere noob"});
+        }
+
+        #region Property
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void Notify(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
