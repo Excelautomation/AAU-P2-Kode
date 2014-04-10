e98d8ccba0177c70771b201795958f1947802fce
Søren Bjergmark <sahb1239@gmail.com>
2014-04-10 12:35:58 -0700

    Fixet MySQL connect

--- ARK/Model/DamageDescription.cs
+++ ARK/Model/DamageDescription.cs
@@ -1,7 +1,11 @@
-﻿namespace ARK.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ARK.Model
 {
     public class DamageDescription
     {
+        [Key]
+        public int ID { get; set; }
         public string Type { get; set; }
         public string Description { get; set; }
         public string NeededMaterials { get; set; }
