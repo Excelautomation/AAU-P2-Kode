98e9287ceaea4a95658e7822d986d1e1b322d272
Mads Edelskjold <Edelskjold@users.noreply.github.com>
2014-04-10 01:11:40 -0700

    DB Setup

---
+++ ARK/Model/DbArkContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data;
+
+
+namespace ARK.Model
+{
+    class DbArkContext : DbContext
+    {
+        DbSet<FtpInfo> FTPInfo { get; set; }
+        DbSet<DamageType> DamageType { get; set; }
+    }
+}
