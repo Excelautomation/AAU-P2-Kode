0f911b2f21129b88ce819088c56b3749fb4259fb
Martin <WIN-BFM6JJ47JRD\Martin>
2014-04-10 01:03:24 -0700

    Liste af medlemmer tilføjet i tur.cs

--- ARK/Model/Tur.cs
+++ ARK/Model/Tur.cs
@@ -19,14 +19,6 @@ namespace ARK.Model
         public DateTime Date { get; set; }
         public bool LongTrip { get; set; }
         public int BoatID { get; set; }
-        public int MemberID_1 { get; set; }
-        public int MemberID_2 { get; set; }
-        public int MemberID_3 { get; set; }
-        public int MemberID_4 { get; set; }
-        public int MemberID_5 { get; set; }
-        public int MemberID_6 { get; set; }
-        public int MemberID_7 { get; set; }
-        public int MemberID_8 { get; set; }
-        public int MemberID_9 { get; set; }
+        public List<Medlem> MembersOnTrip;
     }
 }
