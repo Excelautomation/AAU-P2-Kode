1e4e5944fc1db6e87f6ce10558a02ab7de8a01fb
Søren Bjergmark <sahb1239@gmail.com>
2014-04-10 01:16:46 -0700

    Startet implementation af filter Nuget

---
+++ ARK/Model/Search/CheckboxFilter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ARK.Model.Search
+{
+    public class CheckboxFilter<T> : IFilter<T, CheckBox>
+    {
+        // TODO fix EventArgs til at indeholde active
+        private CheckBox _control;
+
+        public CheckboxFilter(CheckBox checkbox, Func<T, bool> filter)
+        {
+            this.Control = checkbox;
+            this.Filter = filter;
+
+            WrapperEvent += (sender, e) => ActiveChanged(this, new EventArgs());
+        }
+
+        public event EventHandler ActiveChanged;
+        private event RoutedEventHandler WrapperEvent;
+
+        public bool Active
+        {
+            get
+            {
+                return Control.IsChecked.GetValueOrDefault();
+            }
+            set { Control.IsChecked = value; ActiveChanged(this, new EventArgs()); }
+        }
+
+        public CheckBox Control
+        {
+            get { return _control; }
+            private set
+            {
+                if (value == null) throw new ArgumentNullException("Control");
+
+                _control = value;
+                _control.Checked += WrapperEvent;
+            }
+        }
+        public Func<T, bool> Filter { get; private set; }
+    }
+}
