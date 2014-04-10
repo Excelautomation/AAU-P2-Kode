cbb38c7c1ddbc49cc2a1695b7fbe222abc755b9a
Mads <WIN-BFM6JJ47JRD\Mads>
2014-04-10 01:11:40 -0700

    DB Setup

---
+++ ARK/Model/FtpInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARK.Model
+{
+    class FtpInfo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Hostname { get; set; }
+        public string password { get; set; }
+        public int Port { get; set; }
+    }
+}
