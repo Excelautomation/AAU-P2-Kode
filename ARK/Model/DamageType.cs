79763a51e51c78c6e1b720d604b00bcd2b5931bf
Mads Edelskjold <Edelskjold@users.noreply.github.com>
2014-04-10 01:11:40 -0700

    DB Setup

---
+++ ARK/Model/DamageType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARK.Model
+{
+    class DamageType
+    {
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public bool IsFunctional { get; set; }
+    }
+}
