cd8dfe9cba91497b4a71c1340cb813e3b3cb8b11
Martin <WIN-BFM6JJ47JRD\Martin>
2014-04-14 04:38:34 -0700

    FormsOverview items layout tilføjet. Kan ikke finde ud af at få content til at blive venstrecentreret og bevare button :(

--- ARK/ViewModel/FormsViewModel.cs
+++ ARK/ViewModel/FormsViewModel.cs
@@ -1,12 +1,47 @@
-﻿using System.Collections.ObjectModel;
+﻿using ARK.Model;
+using ARK.Model.DB;
+using ARK.Model.Search;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
 using ARK.ViewModel.Base;
 
 namespace ARK.ViewModel
 {
-    public class FormsViewModel : Base.ViewModel, IFilter
+    public class FormsViewModel : INotifyPropertyChanged, IFilter
     {
         private ObservableCollection<Control> _filters;
+
+        private List<DamageForm> _DamageForms = new List<DamageForm>();
+
+
+        public FormsViewModel()
+        {
+            using (var db = new DbArkContext())
+            {
+                DamageForms = new List<DamageForm>(db.DamageForm);
+            }
+
+
+            DamageForms.Add(new DamageForm { DamagedBoat = new Boat() { Name = "Hajen" }, Date = new System.DateTime(2014, 4, 14), ReportedBy = "John Doge Larsen"});
+            DamageForms.Add(new DamageForm { DamagedBoat = new Boat() { Name = "Den Flyvende Hollænder" }, Date = new System.DateTime(2014, 4, 14), ReportedBy = "Davy Jones" });
+            DamageForms.Add(new DamageForm { DamagedBoat = new Boat() { Name = "A Motherfucking Boat" }, Date = new System.DateTime(2014, 4, 14), ReportedBy = "Samuel L. Jackson" });
+            
+        }
+        
+        
+
+        public List<DamageForm> DamageForms
+        {
+            get { return _DamageForms; }
+            set { _DamageForms = value; Notify("DamageForms"); }
+        } 
+
+
+
 
         public ObservableCollection<Control> Filters
         {
@@ -22,5 +57,18 @@ namespace ARK.ViewModel
                 });
             }
         }
+        #region Property
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void Notify(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion Property
     }
 }
