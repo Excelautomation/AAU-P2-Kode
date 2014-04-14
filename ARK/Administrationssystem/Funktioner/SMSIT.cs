cb0791c3f4e663c73b0b15a1998ad6ee96bb1f76
Mads Edelskjold <Edelskjold@users.noreply.github.com>
2014-04-14 04:55:40 -0700

    SMS Gateway

---
+++ ARK/Administrationssystem/Funktioner/SMSIT.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARK.Administrationssystem.Funktioner
+{
+
+    public class SMSIT
+    {
+        private static string url_base;
+
+        public static bool SendSMS(Model.SMS SMSInformation)
+        {
+            url_base = "http://www.smsit.dk/api/sendSms.php";
+            WebClient client = new WebClient();
+            var apikey = "0000000000000000";
+            var senderid = "ARK";
+            var charset = "ISO-8859-1";
+            var url = string.Format("{0}?apiKey={1}&senderId={2}&mobile={3}&message={4}&charset={5}", url_base, apikey, senderid, SMSInformation.Reciever, SMSInformation.Message, charset);
+            return client.DownloadString(url) == "0";
+        }
+    }
+}
