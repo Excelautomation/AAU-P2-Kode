a72f09894da6cba84e78bcc610774cc326678efa
Mads <WIN-BFM6JJ47JRD\Mads>
2014-04-14 23:39:58 -0700

    Added GetSMS

---
+++ ARK/Model/GetSMS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARK.Model
+{
+    public class GetSMS
+    {
+        public int Id { get; set; }
+        public string From { get; set; }
+        public string Text { get; set; }
+        public bool RecievedDate { get; set; }
+    }
+}
