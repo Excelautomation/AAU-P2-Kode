7d6d5bd988d70d1d3b76e17e15f40585e5fc1ee7
Jonas <WIN-BFM6JJ47JRD\Jonas>
2014-04-23 03:43:16 -0700

    code style

--- ARK/Administrationssystem/Funktioner/SMSIT.cs
+++ ARK/Administrationssystem/Funktioner/SMSIT.cs
@@ -7,19 +7,18 @@ using System.Threading.Tasks;
 
 namespace ARK.Administrationssystem.Funktioner
 {
-
     public class SMSIT
     {
-        private static string url_base;
+        private static string urlBase;
 
-        public static bool SendSMS(Model.SMS SMSInformation)
+        public static bool SendSMS(Model.SMS smsInformation)
         {
-            url_base = "http://www.smsit.dk/api/sendSms.php";
+            urlBase = "http://www.smsit.dk/api/sendSms.php";
             WebClient client = new WebClient();
             var apikey = "0000000000000000";
             var senderid = "22821674";
             var charset = "UTF-8";
-            var url = string.Format("{0}?apiKey={1}&senderId={2}&mobile={3}&message={4}&charset={5}", url_base, apikey, senderid, SMSInformation.Reciever, SMSInformation.Message, charset);
+            var url = string.Format("{0}?apiKey={1}&senderId={2}&mobile={3}&message={4}&charset={5}", urlBase, apikey, senderid, smsInformation.Reciever, smsInformation.Message, charset);
             return client.DownloadString(url) == "0";
         }
     }
