9a93351e5af52ad3989d0ac997ba82b3e50dd49b
Jimmi <WIN-BFM6JJ47JRD\Jimmi>
2014-04-23 07:38:43 -0700

    EndTrip påbegyndt

---
+++ ARK/ViewModel/Protokolsystem/EndTripViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ARK.Model;
+using ARK.Model.DB;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARK.ViewModel
+{
+    class EndTripViewModel : Base.ViewModel
+    {
+        private bool _boatsOut;
+
+        public EndTripViewModel()
+        {
+            // Indlæs data
+            using (DbArkContext db = new DbArkContext())
+            {
+                List<Boat> Boats = new List<Boat>(db.Boat);
+            }
+        }
+
+        public bool BoatsOut
+        {
+            get
+            {
+                return _boatsOut;
+            }
+            set
+            {
+                _boatsOut = value;
+                Notify();
+            }
+        }
+    }
+}
