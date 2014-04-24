225d5408cc2fb68436492ffdf523b6e6a8db54ec
Martin <WIN-BFM6JJ47JRD\Martin>
2014-04-24 04:17:52 -0700

    En masse adminboats lavet. Er ikke færdig men det er 6 timer siden jeg sidst checkede in så...

--- ARK/ViewModel/Administrationssystem/BoatViewModel.cs
+++ ARK/ViewModel/Administrationssystem/BoatViewModel.cs
@@ -1,12 +1,72 @@
-﻿using System.Collections.ObjectModel;
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
+using System.Collections.Generic;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+using System.Collections;
+using System.Linq;
+using System.Text.RegularExpressions;
 using ARK.ViewModel.Base;
+using ARK.Model;
+using ARK.Model.DB;
 
 namespace ARK.ViewModel.Administrationssystem
 {
     public class BoatViewModel : Base.ViewModel, IFilter
     {
         private ObservableCollection<Control> _filters;
+        private Boat _Boat;
+        private List<Boat> _Boats = new List<Boat>();
+
+
+        public BoatViewModel()
+        {
+            // Load data
+            using (DbArkContext db = new DbArkContext())
+            {
+                Boats = new List<Boat>(db.Boat).Where(x => x.Active).OrderBy(x => x.NumberofSeats).ToList();
+            }
+
+            if (Boats.Count != 0)
+            {
+                Boat = Boats[0];
+            }
+        }
+
+
+
+
+
+        public List<Boat> Boats
+        {
+            get { return _Boats; }
+            set
+            {
+                _Boats = value;
+                Notify();
+            }
+        }
+
+        public Boat Boat
+        {
+            get { return _Boat; }
+            set
+            {
+                _Boat = value;
+                Notify();
+                //NotifyProp("BoatContent");
+            }
+        }
+
+        public ICommand SelectedChange
+        {
+            get
+            {
+                return GetCommand<Boat>(e =>
+                {
+                    Boat = e;
+                });
+            }
+        }
 
         public ObservableCollection<Control> Filters
         {
