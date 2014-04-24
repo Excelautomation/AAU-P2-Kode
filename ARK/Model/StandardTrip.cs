e15bf3d2552bc2b6f461c8e297111efd43108035
Jonas <WIN-BFM6JJ47JRD\Jonas>
2014-04-24 03:30:16 -0700

    Added standardtrip calss

---
+++ ARK/Model/StandardTrip.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARK.Model
+{
+    public class StandardTrip
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int Distance { get; set; }
+    }
+}
