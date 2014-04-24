74e0a61934b29f9749632002c2174b80c8a40061
Myrionsc2 <marpht@hotmail.com>
2014-04-24 04:17:52 -0700

    En masse adminboats lavet. Er ikke færdig men det er 6 timer siden jeg sidst checkede in så...

--- ARK/ViewModel/Administrationssystem/FormsViewModel.cs
+++ ARK/ViewModel/Administrationssystem/FormsViewModel.cs
@@ -21,49 +21,11 @@ namespace ARK.ViewModel.Administrationssystem
         {
             using (DbArkContext db = new DbArkContext())
             {
-                //DamageForms = new List<DamageForm>(db.DamageForm);
+                DamageForms = new List<DamageForm>(db.DamageForm);
                 //LongDistanceForms = new List<LongDistanceForm>(db.LongTripForm);
             }
 
-            DamageForms.Add(new DamageForm { Boat = new Boat() 
-            {
-                Id = 100,
-                Name = "Hajen",
-                Usable = false,
-                Active = false 
-            },
-                Date = new DateTime(2014, 4, 14),
-                RegisteringMember = new Member() { FirstName = "Kurt", LastName = "Nørremark" },
-                DamageDescription = new DamageDescription() { Description = "SVAB!", Type = "Hul i skroget", Id = 1, NeededMaterials = "Træ, spyt og sæd" }
-            });
-            DamageForms.Add(new DamageForm { Boat = new Boat() 
-            {
-                Id = 200,
-                Name = "Den Flyvende Hollænder",
-                Usable = false,
-                Active = false 
-            },
-                Date = new DateTime(2014, 4, 14),
-                RegisteringMember = new Member() { FirstName = "Davy", LastName = "Jones" },
-                DamageDescription = new DamageDescription() { Description = "Den er fooked OP", Type = "Hul i skroget", Id = 1, NeededMaterials = "Træ, spyt og sæd" }
-            });
-            DamageForms.Add(new DamageForm
-            {
-                Boat = new Boat
-                {
-                    Id = 200,
-                    Name = "A Motherfucking Boat",
-                    Usable = false,
-                    Active = false
-                },
-                Date = new DateTime(2014, 4, 14),
-                RegisteringMember = new Member() { FirstName = "Samuel", LastName = "L. Jackson" },
-                DamageDescription = new DamageDescription() { Description = "Den er fooked OP", Type = "Hul i skroget", Id = 1, NeededMaterials = "Træ, spyt og sæd" }
-            });
-            
-            LongDistanceForms.Add(new LongDistanceForm { Departure = new DateTime(2014, 4, 14), Arrival = new DateTime(2024, 4, 14) });
-
-            Page = new FormsDamage { DataContext = new FormsDamageViewModel { DamageForm = DamageForms[0] } };
+            //Page = new FormsDamage { DataContext = new FormsDamageViewModel { DamageForm = DamageForms[0] } };
         }       
 
         public UserControl Page { 
