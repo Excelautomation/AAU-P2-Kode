4cd1d797d06e4a215592c49a7b8dedb158a1fe3d
Jonas <WIN-BFM6JJ47JRD\Jonas>
2014-04-24 14:03:45 -0700

    added properties

--- ARK/Model/DamageForm.cs
+++ ARK/Model/DamageForm.cs
@@ -6,13 +6,17 @@ namespace ARK.Model
     {
         public int Id { get; set; }
         public DateTime Date { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public string NeededMaterials { get; set; }
+        public bool Closed { get; set; }
 
+        //Foreign Keys
         public int RegisteringMemberId { get; set; }
-        public virtual Member RegisteringMember { get; set; }
-
         public int BoatId { get; set; }
+        
+        //Navigation properties
         public virtual Boat Boat { get; set; }
-
-        public virtual DamageDescription DamageDescription { get; set; }
+        public virtual Member RegisteringMember { get; set; }
     }
 }
