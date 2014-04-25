8d80785375fe82a108d8e9a64e8d8810b9cccac0
Jonas <WIN-BFM6JJ47JRD\Jonas>
2014-04-25 05:23:02 -0700

    Fixed admin and member

--- ARK/Model/Admin.cs
+++ ARK/Model/Admin.cs
@@ -8,6 +8,7 @@ namespace ARK.Model
 {
     public class Admin
     {
+        public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public bool Contact { get; set; }
