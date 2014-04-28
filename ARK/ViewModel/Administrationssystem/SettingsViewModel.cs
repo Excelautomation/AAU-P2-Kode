ddf4b445bd57ea3058aa81f7eac838a394c81d3e
Søren Bjergmark <sahb1239@gmail.com>
2014-04-28 03:51:16 -0700

    Settings

--- ARK/ViewModel/Administrationssystem/SettingsViewModel.cs
+++ ARK/ViewModel/Administrationssystem/SettingsViewModel.cs
@@ -1,11 +1,27 @@
 ﻿using System.Collections.ObjectModel;
 using System.Windows.Controls;
+using ARK.Model;
+using ARK.Model.DB;
 using ARK.ViewModel.Base;
 
 namespace ARK.ViewModel.Administrationssystem
 {
     public class SettingsViewModel : ContentViewModelBase
     {
+        private ObservableCollection<DamageType> _damageTypes;
 
+        public SettingsViewModel()
+        {
+            using (var dbcontext = new DbArkContext())
+            {
+                DamageTypes = new ObservableCollection<DamageType>(dbcontext.DamageType);
+            }
+        }
+
+        public ObservableCollection<DamageType> DamageTypes
+        {
+            get { return _damageTypes; }
+            set { _damageTypes = value; Notify(); }
+        }
     }
 }
