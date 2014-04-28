cd2feaac4fe0ec26a4cf464e25e4756ee9e50433
Rune <WIN-BFM6JJ47JRD\Rune>
2014-04-28 05:56:19 -0700

    Skadetyper i SettingsVM påbegyndt

--- ARK/ViewModel/Administrationssystem/SettingsViewModel.cs
+++ ARK/ViewModel/Administrationssystem/SettingsViewModel.cs
@@ -1,15 +1,48 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows.Controls;
+using System.Windows.Input;
+using ARK.Extensions;
 using ARK.Model;
 using ARK.Model.DB;
-using ARK.ViewModel.Base;
+using ARK.Model.Search;
 
 namespace ARK.ViewModel.Administrationssystem
 {
     public class SettingsViewModel : ContentViewModelBase
     {
-        private ObservableCollection<DamageType> _damageTypes;
 
+
+        #region Skadetyper
+        private ObservableCollection<DamageType> _damageTypes;
+        private DamageType _CurrentDamageType;
+
+        public ObservableCollection<DamageType> DamageTypes
+        {
+            get { return _damageTypes; }
+            set { _damageTypes = value; Notify(); }
+        }
+
+        public DamageType CurrentDamageType
+        {
+            get { return _CurrentDamageType; }
+            set { _CurrentDamageType = value; Notify(); }
+        }
+
+        public ICommand SelectedChange
+        {
+            get
+            {
+                return GetCommand<DamageType>(e => { CurrentDamageType = e; });
+            }
+        }
+
+
+        #endregion
+
+        #region Constructor
         public SettingsViewModel()
         {
             using (var dbcontext = new DbArkContext())
@@ -17,11 +50,6 @@ namespace ARK.ViewModel.Administrationssystem
                 DamageTypes = new ObservableCollection<DamageType>(dbcontext.DamageType);
             }
         }
-
-        public ObservableCollection<DamageType> DamageTypes
-        {
-            get { return _damageTypes; }
-            set { _damageTypes = value; Notify(); }
-        }
+        #endregion
     }
 }
