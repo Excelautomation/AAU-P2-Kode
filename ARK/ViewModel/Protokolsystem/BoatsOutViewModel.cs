02fa38bddf3c324ed6d0f4815bd8f83fff22db73
Jimmi <WIN-BFM6JJ47JRD\Jimmi>
2014-04-29 05:43:11 -0700

    BOatsViewModelBase slettet. Implementeret tilbage i BOatsOutViewModel & BoatsOutViewModel

--- ARK/ViewModel/Protokolsystem/BoatsOutViewModel.cs
+++ ARK/ViewModel/Protokolsystem/BoatsOutViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using ARK.Model;
+using ARK.Model.DB;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,27 @@ using System.Threading.Tasks;
 
 namespace ARK.ViewModel.Protokolsystem
 {
-    class BoatsOutViewModel : BoatsViewModelBase
+    class BoatsOutViewModel : KeyboardContentViewModelBase
     {
+        private List<Boat> _boatsOut = new List<Boat>();
 
+        public BoatsOutViewModel()
+        {
+            using (DbArkContext db = new DbArkContext())
+            {
+                BoatsOut = db.Boat.Where(boat => boat.BoatOut == false)
+                    .OrderByDescending(boat => boat.Trips.FirstOrDefault(trip => trip.TripEndedTime == null).TripStartTime).ToList();
+            }
+        }
+
+        public List<Boat> BoatsOut
+        {
+            get { return _boatsOut; }
+            set
+            {
+                _boatsOut = value;
+                Notify();
+            }
+        }
     }
 }
