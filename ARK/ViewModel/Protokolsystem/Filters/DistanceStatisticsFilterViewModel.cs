7bc426f6149b9a2fb2be7855cda9f7ece17e1c91
sahb <WIN-BFM6JJ47JRD\sahb>
2014-04-30 13:56:54 -0700

    Oprettet viewmodels for filters

---
+++ ARK/ViewModel/Protokolsystem/Filters/DistanceStatisticsFilterViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ARK.ViewModel.Base;
+using ARK.ViewModel.Base.Interfaces.Filter;
+
+namespace ARK.ViewModel.Protokolsystem.Filters
+{
+    internal class DistanceStatisticsFilterViewModel : ViewModelBase, IFilterViewModel
+    {
+        public IFilterContainerViewModel FilterContainer { get; set; }
+
+        public event EventHandler FilterChanged;
+    }
+}
