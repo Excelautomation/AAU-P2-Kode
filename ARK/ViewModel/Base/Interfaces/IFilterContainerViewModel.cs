8b84407093dfe59d3854777c469af582830de2ea
Søren Bjergmark <sahb1239@gmail.com>
2014-04-30 06:03:18 -0700

    Fix namespaces

--- ARK/ViewModel/Base/Interfaces/IFilterContainerViewModel.cs
+++ ARK/ViewModel/Base/Interfaces/IFilterContainerViewModel.cs
@@ -1,26 +1,16 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.Windows;
+using ARK.ViewModel.Base.Filter;
 
 namespace ARK.ViewModel.Base.Interfaces
 {
-    public class SearchEventArgs : EventArgs
-    {
-        public string SearchText { get; private set; }
-
-        public SearchEventArgs(string searchText)
-        {
-            this.SearchText = searchText;
-        }
-    }
-
     public interface IFilterContainerViewModel : IViewModelBase
     {
-        event EventHandler<SearchEventArgs> SearchTextChanged;
-
         bool EnableSearch { get; set; }
         bool EnableFilters { get; set; }
 
         ObservableCollection<FrameworkElement> Filters { get; set; }
+        event EventHandler<SearchEventArgs> SearchTextChanged;
     }
 }
