c9505b7aa3fa7f1b35e9499a2b0d283904c705d9
Jimmi <WIN-BFM6JJ47JRD\Jimmi>
2014-04-30 12:08:47 -0700

    DistanceStatistiksFilters made

---
+++ ARK/View/Protokolsystem/Filters/DistanceStatisticsFilters.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ARK.View.Protokolsystem.Filters
+{
+    /// <summary>
+    /// Interaction logic for OnScreenFilters.xaml
+    /// </summary>
+    public partial class DistanceStatisticsFilters : UserControl
+    {
+        public DistanceStatisticsFilters()
+        {
+            InitializeComponent();
+        }
+    }
+}
