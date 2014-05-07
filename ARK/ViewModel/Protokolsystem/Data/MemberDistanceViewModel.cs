d132f5d15d94e1c18f6b9ef8ad905be1d718dc50
sahb <WIN-BFM6JJ47JRD\sahb>
2014-05-07 04:10:44 -0700

    Renames

---
+++ ARK/ViewModel/Protokolsystem/Data/MemberDistanceViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ARK.Model;
+using ARK.ViewModel.Base;
+
+namespace ARK.ViewModel.Protokolsystem.Data
+{
+    class MemberDistanceViewModel : ViewModelBase
+    {
+        private Member _member;
+        private double _distance;
+
+        public MemberDistanceViewModel(Member member, double distance)
+        {
+            this.Member = member;
+            this.Distance = distance;
+        }
+
+        public Member Member
+        {
+            get { return _member; }
+            set { _member = value;
+                Notify();
+            }
+        }
+
+        public double Distance
+        {
+            get { return _distance; }
+            set { _distance = value;
+                Notify();
+            }
+        }
+    }
+}
