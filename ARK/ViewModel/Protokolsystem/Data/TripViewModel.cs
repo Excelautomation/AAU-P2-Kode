5722b7a077b601d6b540458a4c14d8d2bea624a0
Jonas <WIN-BFM6JJ47JRD\Jonas>
2014-05-08 02:42:31 -0700

    Fixed editable

---
+++ ARK/ViewModel/Protokolsystem/Data/TripViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using ARK.Model;
+using ARK.ViewModel.Base;
+
+namespace ARK.ViewModel.Protokolsystem.Data
+{
+    public class TripViewModel : ViewModelBase
+    {
+        private Trip _trip;
+
+        public TripViewModel(Trip trip)
+        {
+            this.Trip = trip;
+        }
+
+        public Trip Trip
+        {
+            get { return _trip; }
+            private set
+            {
+                this._trip = value;
+                Notify();
+            }
+        }
+
+        public bool Editable
+        {
+            get
+            {
+                return this.Trip.TripEndedTime != null &&
+                       (DateTime.Now - this.Trip.TripEndedTime.Value).TotalMinutes < 30;
+            }
+        }
+    }
+}
