e03170a783f351ce09e09d16b55741062099f93b
Jimmi <WIN-BFM6JJ47JRD\Jimmi>
2014-05-08 11:00:19 -0700

    Langturs additional View lavet

---
+++ ARK/ViewModel/Protokolsystem/Additional/CreateLongTripFormAdditionalInfoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using System.Windows.Input;
+using ARK.Model;
+using ARK.ViewModel.Base;
+using ARK.ViewModel.Base.Interfaces;
+using System.Collections.ObjectModel;
+
+namespace ARK.ViewModel.Protokolsystem.Additional
+{
+    public class CreateLongTripFormAdditionalInfoViewModel : ContentViewModelBase
+    {
+        // Fields
+
+    }
+}
