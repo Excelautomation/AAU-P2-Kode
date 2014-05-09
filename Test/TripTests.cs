d6c1519b642397f757869582ca3532f3e7ee973a
Mikkel Christensen <mikkel@mclc.dk>
2014-05-09 04:40:38 -0700

    +1 unittest til Boat, +1 unittest til Trip

---
+++ Test/TripTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ARK.Model;
+
+namespace Test
+{
+    [TestClass]
+    public class TripTests
+    {
+        [TestMethod]
+        public void TimeBoatOut_Wait5Secs_Return5()
+        {
+            // Arrange
+            Trip trip = new Trip();
+            trip.TripStartTime = DateTime.Now;
+            int expected = 5;
+
+            // Act
+            System.Threading.Thread.Sleep(5000);
+            int actual = (int)(DateTime.Now - trip.TripStartTime).TotalSeconds;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
