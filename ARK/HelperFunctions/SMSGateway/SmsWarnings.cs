e65d503b10b1402bcb8c860b87f16c7b215da59d
Søren Bjergmark <sahb1239@gmail.com>
2014-05-11 08:12:03 -0700

    Opdateret SMS gateway

--- ARK/HelperFunctions/SMSGateway/SmsWarnings.cs
+++ ARK/HelperFunctions/SMSGateway/SmsWarnings.cs
@@ -5,6 +5,7 @@ using System.Data.Entity;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using ARK.Model;
 using ARK.Model.DB;
@@ -12,17 +13,91 @@ using ARK.Model.XML;
 
 namespace ARK.HelperFunctions.SMSGateway
 {
+    public class DemoSMSGateway : ISmsGateway
+    {
+
+        public bool SendSms(string sender, string reciever, string message)
+        {
+            Debug.WriteLine("Sender sms til " + reciever + " fra " + sender + " med beskeden " + message);
+            return true;
+        }
+    }
+
     public class SmsWarnings
     {
+        private const string Sender = "ARK";
+        private const string MessageNotHome =
+            "Hej {0} Bekræft venligst med OK, at du har det godt. Venlig hilsen Aalborg Roklub";
+        private const string MessageInvalidResponse = "Bekræftelsen blev ikke modtaget";
+        private const string MessageValidResponse = "Bekræftelsen blev succesfuldt modtaget";
+
+        public SmsWarnings()
+        {
+            Gateway = new DemoSMSGateway();
+        }
+
         public ISmsGateway Gateway { get; set; }
 
         public void Test()
         {
+            // Gør kun dette her når solen er nede
+
             using (var db = new DbArkContext())
             {
                 var warnings = GetTripWarningSms(db).ToList();
+                var responses = db.GetSMS.ToList();
+
+                HandleWarningSms(warnings);
+                HandleResponseSms(warnings, responses);
+
+                // Fjern tidligere sms'er
+                db.GetSMS.RemoveRange(responses);
+
+                db.SaveChanges();
             }
         }
+
+        private void HandleResponseSms(IEnumerable<TripWarningSms> warnings, IEnumerable<GetSMS> responses)
+        {
+            foreach (var response in responses)
+            {
+                var warn =
+                    warnings
+                        .Where(warning => warning.Trip.Members
+                            .Any(member => member.Phone == response.From.Replace("+45", "")))
+                        .ToList();
+
+                if (warn.Any())
+                {
+                    if (response.Text.ToLower() == "ok")
+                    {
+                        warn[0].RecievedSms = response.RecievedDate;
+                        Gateway.SendSms(Sender, response.From.Replace("+45", ""), MessageValidResponse);
+                    }
+                    else
+                    {
+                        Gateway.SendSms(Sender, response.From.Replace("+45", ""), MessageInvalidResponse);
+                    }
+                }
+            }
+        }
+
+        private void HandleWarningSms(IEnumerable<TripWarningSms> warnings)
+        {
+            var pending = warnings
+                .Where(warning => warning.SentSms == null);
+
+            foreach (var sms in pending)
+            {
+                foreach (var member in sms.Trip.Members.Where(member => !string.IsNullOrEmpty(member.Phone)))
+                {
+                    Gateway.SendSms(Sender, member.Phone, string.Format(MessageNotHome,
+                        member.FirstName + " " + member.LastName));
+                }
+                sms.SentSms = DateTime.Now;
+            }
+        }
+
         private IEnumerable<TripWarningSms> GetTripWarningSms(DbArkContext db)
         {
             // Find all trips which dosn't have a tripwarningsms yet - select tripWarningSms and add to db
@@ -46,12 +121,9 @@ namespace ARK.HelperFunctions.SMSGateway
             if (endedTrips.Any())
                 db.TripWarningSms.RemoveRange(endedTrips);
 
-            // Save
-            if (missingTrips.Any() || endedTrips.Any())
-                db.SaveChanges();
-
             return db.TripWarningSms
                 .Where(warning => warning.RecievedSms == null)
+                .Where(warning => warning.Trip.TripEndedTime == null)
                 .Include(warning => warning.Trip);
         }
 
