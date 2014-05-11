314092543c57ebc8f480489384b4a1c7964238eb
Jimmi <WIN-BFM6JJ47JRD\Jimmi>
2014-05-11 12:06:24 -0700

    AdminLogin lavet i confirmation-vindue, controltemplate for button udvidet med greyout, hvis de er disabled

---
+++ ARK/View/Protokolsystem/Confirmations/AdminLogin.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ARK.View.Protokolsystem.Confirmations
+{
+	/// <summary>
+	/// Interaction logic for BeginTripBoatsConfirm.xaml
+	/// </summary>
+	public partial class AdminLogin : UserControl
+	{
+        public AdminLogin()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
