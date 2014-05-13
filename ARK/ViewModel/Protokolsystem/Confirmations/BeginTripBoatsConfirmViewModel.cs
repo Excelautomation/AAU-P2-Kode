17a8b077a35687a6c1e2759de6bf1f3306264129
Martin <WIN-BFM6JJ47JRD\Martin>
2014-05-13 08:09:32 -0700

    begintripConfirm Dialogbox

--- ARK/ViewModel/Protokolsystem/Confirmations/BeginTripBoatsConfirmViewModel.cs
+++ ARK/ViewModel/Protokolsystem/Confirmations/BeginTripBoatsConfirmViewModel.cs
@@ -4,12 +4,47 @@ using ARK.Model;
 using ARK.ViewModel.Base;
 using ARK.ViewModel.Base.Interfaces;
 using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace ARK.ViewModel.Protokolsystem.Confirmations
 {
     public class BeginTripBoatsConfirmViewModel : ConfirmationViewModelBase
     {
         // Fields
+        private Trip _trip;
+        
+        
+        //BeginTripBoatsConfirmViewModel()
+        //{
 
+        //}
+
+
+
+
+
+
+
+        //_db.Trip.Add(trip);
+        //_db.SaveChanges();
+
+        //ResetData();
+
+        public ICommand CloseWindow
+        {
+            get
+            {
+                return GetCommand<object>(e =>
+                {
+                    
+                });
+            }
+        }
+
+        public Trip Trip
+        {
+            get { return _trip; }
+            set { _trip = value; Notify(); }
+        }
     }
 }
