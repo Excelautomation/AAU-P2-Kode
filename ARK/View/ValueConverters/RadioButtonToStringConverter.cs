67b9bdba377365164fd04edd97ae00bfd2f3df85
Myrionsc2 <marpht@hotmail.com>
2014-05-13 08:09:32 -0700

    begintripConfirm Dialogbox

---
+++ ARK/View/ValueConverters/RadioButtonToStringConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace ARK.View.ValueConverters
+{
+    class RadioButtonToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value != null)
+                return value.Equals(parameter);
+            else
+                return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value.Equals(true) ? parameter : Binding.DoNothing;
+        }
+    }
+}
