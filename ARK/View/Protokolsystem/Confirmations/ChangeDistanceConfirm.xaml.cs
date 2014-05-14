7aa5d322f1062936e6eca53881c8f4e0f14c6070
Jimmi <WIN-BFM6JJ47JRD\Jimmi>
2014-05-13 23:53:48 -0700

    ChangeDistanceConfirm View lavet

---
+++ ARK/View/Protokolsystem/Confirmations/ChangeDistanceConfirm.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ARK.View.Protokolsystem.Confirmations
+{
+	/// <summary>
+	/// Interaction logic for ChangeDistanceConfirm.xaml
+	/// </summary>
+	public partial class ChangeDistanceConfirm : UserControl
+	{
+        public ChangeDistanceConfirm()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
