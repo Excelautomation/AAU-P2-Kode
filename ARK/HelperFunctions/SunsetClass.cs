60649d093c34a8e4ab72892b68fdf3c6cc9c54f6
Jonas <WIN-BFM6JJ47JRD\Jonas>
2014-05-14 08:00:39 -0700

    Finished sunsetclass

--- ARK/HelperFunctions/SunsetClass.cs
+++ ARK/HelperFunctions/SunsetClass.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using ARK.Model.XML;
 
@@ -9,15 +10,32 @@ namespace ARK.HelperFunctions
 {
     public static class SunsetClass
     {
+        private static Task _task;
         private static DateTime _sunset;
 
         public static DateTime Sunset
         {
-            get
+            get { return _sunset != default(DateTime) ? _sunset : (_sunset = XmlParser.GetSunsetFromXml()); }
+            private set { _sunset = value; }
+        }
+
+        internal static void StartSunsetTask(CancellationTokenSource wtoken)
+        {
+            if (_task == null)
             {
-                return _sunset != DateTime.MinValue && _sunset.Date == DateTime.Now.Date
-                    ? _sunset
-                    : (_sunset = XmlParser.GetSunsetFromXml());
+                _task = Task.Factory.StartNew(async () =>
+                {
+                    while (true)
+                    {
+                        Sunset = XmlParser.GetSunsetFromXml();
+                        var delay = DateTime.Today.AddDays(1).AddHours(2) - DateTime.Now;
+                        await Task.Delay(delay, wtoken.Token);
+                    }
+                }, wtoken.Token);
+            }
+            else
+            {
+                throw new InvalidOperationException("The task has already been started");
             }
         }
     }
