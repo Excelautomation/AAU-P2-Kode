acc661eb4d667cb5814d1c6919273074b75c6bac
Søren Bjergmark <sahb1239@gmail.com>
2014-05-14 05:17:48 -0700

    Fixet smsit

--- ARK/HelperFunctions/SMSGateway/SMSIT.cs
+++ ARK/HelperFunctions/SMSGateway/SMSIT.cs
@@ -22,7 +22,7 @@ namespace ARK.HelperFunctions.SMSGateway
 
         public bool SendSms(string sender, string reciever, string message)
         {
-            string url = string.Format("{0}?apiKey={1}&senderId={2}&mobile={3}&message={4}&charset={5}",
+            string url = string.Format("{0}?apiKey={1}&senderId={2}&mobile=45{3}&message={4}&charset={5}",
                 UrlBase, ApiKey, sender, reciever, message, CharSet);
 
             var client = new WebClient();
