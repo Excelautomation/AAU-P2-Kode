8a73f0b135256660062bab245df52af7192f6d48
Myrionsc2 <marpht@hotmail.com>
2014-05-14 03:47:40 -0700

    Alle Confirmations er færdige

--- ARK/ViewModel/Protokolsystem/Confirmations/CreateLongTripConfirmViewModel.cs
+++ ARK/ViewModel/Protokolsystem/Confirmations/CreateLongTripConfirmViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using System.Windows.Input;
 using ARK.Model;
+using ARK.Model.DB;
 using ARK.ViewModel.Base;
 using ARK.ViewModel.Base.Interfaces;
 using System.Collections.ObjectModel;
@@ -10,6 +11,50 @@ namespace ARK.ViewModel.Protokolsystem.Confirmations
     public class CreateLongTripConfirmViewModel : ConfirmationViewModelBase
     {
         // Fields
+        private LongTripForm _longTrip;
 
+        public LongTripForm LongTrip 
+        {
+            get { return _longTrip; }
+            set { _longTrip = value; Notify(); }
+        }
+
+        public ICommand SaveForm
+        {
+            get
+            {
+                return GetCommand(() =>
+                {
+                    DbArkContext.GetDbContext().LongTripForm.Add(LongTrip);
+                    DbArkContext.GetDbContext().SaveChanges();
+                    
+                    Hide();
+                    ProtocolSystem.StatisticsDistance.Execute(null);
+                });
+            }
+        }
+
+        public ICommand CancelForm
+        {
+            get
+            {
+                return GetCommand(() =>
+                {
+                    Hide();
+                    ProtocolSystem.StatisticsDistance.Execute(null);
+                });
+            }
+        }
+
+        public ICommand ChangeForm
+        {
+            get
+            {
+                return GetCommand(() =>
+                {
+                    Hide();
+                });
+            }
+        }
     }
 }
