c46dbc24f81fe2ec3ab23656ec8d54e0e6b34a0f
Jonas <WIN-BFM6JJ47JRD\Jonas>
2014-05-13 23:59:58 -0700

    Added static sunset var Removed newline from direction

---
+++ ARK/HelperFunctions/Sunset.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ARK.Model.XML;
+
+namespace ARK.HelperFunctions
+{
+    public static class Sunset
+    {
+        private static DateTime _sunset;
+
+        public static DateTime GetSunset
+        {
+            get { return _sunset != DateTime.MinValue ? _sunset : (_sunset = XmlParser.GetSunsetFromXml()); }
+        }
+    }
+}
