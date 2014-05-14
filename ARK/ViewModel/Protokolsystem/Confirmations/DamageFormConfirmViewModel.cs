fbf572a086f215e9b89bf94c7bacba8a946aa308
Martin <WIN-BFM6JJ47JRD\Martin>
2014-05-14 02:16:23 -0700

    Opret skade confirmdialog færdig

--- ARK/ViewModel/Protokolsystem/Confirmations/DamageFormConfirmViewModel.cs
+++ ARK/ViewModel/Protokolsystem/Confirmations/DamageFormConfirmViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using System.Windows.Input;
 using ARK.Model;
+using ARK.Model.DB;
 using ARK.ViewModel.Base;
 using ARK.ViewModel.Base.Interfaces;
 using System.Collections.ObjectModel;
@@ -10,6 +11,58 @@ namespace ARK.ViewModel.Protokolsystem.Confirmations
     public class DamageFormConfirmViewModel : ConfirmationViewModelBase
     {
         // Fields
+        private readonly DbArkContext db = DbArkContext.GetDbContext();
+        private DamageForm _damageForm;
+        private string _comment;
 
+        public DamageForm DamageForm
+        { 
+            get { return _damageForm; }
+            set { _damageForm = value; Notify(); }
+        }
+
+        public string Comment
+        {
+            get { return _comment; }
+            set { _comment = value; Notify(); }
+        }
+
+        public ICommand Save
+        {
+            get
+            {
+                return GetCommand<object>(e =>
+                {
+                    DamageForm.Description = string.Concat(DamageForm.Description, "\n\nKommentar\n", Comment);
+                    db.DamageForm.Add(DamageForm);
+                    db.SaveChanges();
+                    Hide();
+                    ProtocolSystem.ViewDamageForm.Execute(null);
+                });
+            }
+        }
+
+        public ICommand Cancel
+        {
+            get
+            {
+                return GetCommand<object>(e =>
+                {
+                    Hide();
+                    ProtocolSystem.StatisticsDistance.Execute(null);
+                });
+            }
+        }
+
+        public ICommand ChangeForm
+        {
+            get
+            {
+                return GetCommand<object>(e =>
+                {
+                    Hide();
+                });
+            }
+        }
     }
 }
