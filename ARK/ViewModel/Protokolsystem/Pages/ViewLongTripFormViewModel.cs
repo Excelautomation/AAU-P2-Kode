d0fd1db7934ea92fa7616e830fe45cda270546cd
yakitawa <rune.willum.larsen@gmail.com>
2014-09-09 13:09:58 +0200

    LongTrip begynd-knap oprettet. Confirm ikke færdig.

--- ARK/ViewModel/Protokolsystem/Pages/ViewLongTripFormViewModel.cs
+++ ARK/ViewModel/Protokolsystem/Pages/ViewLongTripFormViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Input;
@@ -6,14 +7,19 @@ using System.Windows.Input;
 using ARK.Model;
 using ARK.Model.DB;
 using ARK.View.Protokolsystem.Additional;
+using ARK.View.Protokolsystem.Confirmations;
 using ARK.View.Protokolsystem.Pages;
+using ARK.ViewModel.Base;
 using ARK.ViewModel.Protokolsystem.Additional;
+using ARK.ViewModel.Protokolsystem.Confirmations;
 
 namespace ARK.ViewModel.Protokolsystem.Pages
 {
     internal class ViewLongTripFormViewModel : ProtokolsystemContentViewModelBase
     {
         // Fields
+        private readonly DbArkContext db = DbArkContext.GetDbContext(); // Database
+
         private FrameworkElement _infoPage;
 
         private List<LongTripForm> _longTripForms;
@@ -99,6 +105,37 @@ namespace ARK.ViewModel.Protokolsystem.Pages
             get
             {
                 return _infoPage ?? (_infoPage = new ViewLongTripFormAdditionalInfo());
+            }
+        }
+
+        // ConfirmWindow
+        public ICommand ShowConfirmDialog
+        {
+            get
+            {
+                return new RelayCommand(
+                    x => ConfirmTripData(),
+                    x => SelectedLongTripForm != null);
+            }
+        }
+
+        public void ConfirmTripData()
+        {
+            {
+                Trip trip = new Trip
+                {
+                    //Id = db.Trip.OrderByDescending(t => t.Id).First().Id + 1,
+                    TripStartTime = DateTime.Now,
+                    //Members = new List<Member>(SelectedMembers.Select(member => member.Member)),
+                    //Boat = SelectedBoat,
+                    LongTrip = true,
+                    Direction = "Testretning"
+                };
+
+                var dlg = new BeginTripBoatsConfirm();
+                var confirmTripViewModel = (BeginTripBoatsConfirmViewModel)dlg.DataContext;
+                confirmTripViewModel.Trip = trip;
+                ProtocolSystem.ShowDialog(dlg);
             }
         }
 
