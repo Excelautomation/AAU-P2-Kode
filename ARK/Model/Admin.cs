fb207d2cd833527ac8c571221d07ffaa90e908d3
yakitawa <rune.willum.larsen@gmail.com>
2014-09-25 10:24:48 +0200

    Kontakt ved skade i administrator

--- ARK/Model/Admin.cs
+++ ARK/Model/Admin.cs
@@ -10,6 +10,8 @@ namespace ARK.Model
 
         public bool ContactTrip { get; set; }
 
+        public bool ContactDamage { get; set; }
+
         // Navigation property
         public virtual Member Member { get; set; }
 
