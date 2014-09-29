767920808395c2a829a14fc4660a454b894063c6
Myrionsc2 <marpht@hotmail.com>
2014-09-29 14:38:05 +0200

    søren siger jeg skal. Nyt valg af medlemmer vindue.

---
+++ ARK/View/Administrationssystem/Pages/TripMembersWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ARK.View.Administrationssystem.Pages
+{
+    /// <summary>
+    /// Interaction logic for TripMembersWindow.xaml
+    /// </summary>
+    public partial class TripMembersWindow : Window
+    {
+        public TripMembersWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnManipulationBoundaryFeedback(object sender, System.Windows.Input.ManipulationBoundaryFeedbackEventArgs e)
+        {
+            e.Handled = true;
+        }
+    }
+}
