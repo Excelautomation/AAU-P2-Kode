3a67c7920112d4559ea8ebe3bf2a1925a9ad2ddb
MadsGadeberg <Mads.Gadeberg@gmail.com>
2015-04-01 19:47:03 +0200

    api project created

---
+++ Api/Controllers/TripController.cs
@@ -0,0 +1,41 @@
+﻿using ARK.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Api.Controllers
+{
+    public class TripController : ApiController
+    {
+        // GET: api/Trip
+        public IEnumerable<Trip> Get()
+        {
+            //return new string[] { "value1", "value2" };
+            return null;
+        }
+
+        // GET: api/Trip/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Trip
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Trip/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Trip/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
